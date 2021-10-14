de28c7915faa3b8d3c8f0b0e05bb024a035d96e2
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-10-13 22:23:44 -0500

    added quiz questions with answers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-// List of HTML elements for manipulation
+// List of HTML id's for manipulation
 var quizbody = document.getElementById("quiz");
 var resultsEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("finalScore");
@@ -18,3 +18,40 @@ var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
+
+// List of the code quiz questions seen in the mock-up
+var quizQuestions = [{
+    question: "Commonly used data types DO NOT include:",
+    choiceA: "strings",
+    choiceB: "booleans",
+    choiceC: "alerts",
+    choiceD: "numbers",
+    correctAnswer: "c"},
+{
+    question: "The condition in an if / else statement is enclosed with ______.",
+    choiceA: "quotes",
+    choiceB: "curly brackets",
+    choiceC: "parenthesis", 
+    choiceD: "square brackets",
+    correctAnswer: "b"},
+{
+    question: "Arrays in JavaScript can be used to store ______.",
+    choiceA: "numbers and strings",
+    choiceB: "other arrays",
+    choiceC: "booleans", 
+    choiceD: "all of the above",
+    correctAnswer: "d"},
+{
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    choiceA: "commas",
+    choiceB: "curly brackets",
+    choiceC: "quotes", 
+    choiceD: "parenthesis",
+    correctAnswer: "c"},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choiceA: "JavaScript",
+    choiceB: "terminal/bash",
+    choiceC: "for loops", 
+    choiceD: "console log",
+    correctAnswer: "d"}];
